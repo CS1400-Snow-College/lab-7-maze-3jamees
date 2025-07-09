d84b2fe4674f80699e2a01e1818bb3c00559f9b1
3jamees <jamesiiking8@gmail.com>
2025-07-09 18:07:43 -0400

    Added file information at the top and game description

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Author: James King II
+// Date: July 8, 2025
+// Lab name: Lab 7 - Maze 
+
+{
+
+    Console.WriteLine("This is a simple maze game.");
+    Console.WriteLine("Use the arrow keys to move. Try to reach the * to win!");
+    Console.WriteLine("Press any key to start...");
+    Console.ReadKey();
+    
