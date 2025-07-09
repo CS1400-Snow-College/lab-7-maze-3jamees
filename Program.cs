31fd0c878a91945214637f0405f848b60e2a22c5
3jamees <jamesiiking8@gmail.com>
2025-07-09 18:29:39 -0400

    finished

--- Program.cs
+++ Program.cs
@@ -14,7 +14,59 @@
     Console.Clear();
     for (int i = 0; i < mapRows.Length; i++)
     {
-            Console.WriteLine(mapRows[i]);
+        Console.WriteLine(mapRows[i]);
     }
-    
+
+    Console.SetCursorPosition(0, 0);
+    ConsoleKey key = ConsoleKey.NoName;
+    while (key != ConsoleKey.Escape)
+    {
+        key = Console.ReadKey(true).Key;
+
+        int newTop = Console.CursorTop;
+        int newLeft = Console.CursorLeft;
+
+        if (key == ConsoleKey.UpArrow)
+        {
+            newTop = newTop - 1;
+        }
+        else if (key == ConsoleKey.DownArrow)
+        {
+            newTop = newTop + 1;
+        }
+        else if (key == ConsoleKey.LeftArrow)
+        {
+            newLeft = newLeft - 1;
+        }
+        else if (key == ConsoleKey.RightArrow)
+        {
+            newLeft = newLeft + 1;
+        }
+
+        TryMove(newTop, newLeft, mapRows);
+
+        if (mapRows[Console.CursorTop][Console.CursorLeft] == '*')
+        {
+            Console.Clear();
+            Console.WriteLine("You win! Great job!");
+            break;
+        }
+    }
+}
+
+static void TryMove(int top, int left, string[] maze)
+{
+    if (top < 0) return;
+    if (top >= maze.Length) return;
+
+    if (left < 0) return;
+    if (left >= maze[top].Length) return;
+
+    if (maze[top][left] == '#') return;
+
+    Console.SetCursorPosition(left, top);
+}
+
+
+
 
